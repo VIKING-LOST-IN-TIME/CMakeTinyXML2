2a19e785999e7e5277cbc2cfb1df069398c93bea
Tjeu Foolen <tjeufoolen@tjeus-mbp.lan>
2020-09-09 18:32:49 +0200

    basic tinyxml2 example

---
+++ README.md
@@ -0,0 +1,13 @@
+# CMakeTinyXML2
+
+## Prerequisites
+- g++
+- cmake >= 3.18
+
+## Getting Started
+1. Clone this repository.
+2. Fetch all submodules.
+3. Configure, build and run! :tada:
+
+## Author
+- [Tjeu Foolen](https://github.com/tjeufoolen)
--- main.cpp
+++ main.cpp
@@ -1,7 +1,28 @@
 #include <iostream>
+#include <tinyxml2.h>
 
 int main(int argc, char* argv[])
 {
-    std::cout << "Hello, World!" << std::endl;
-    return 0;
+    const char* filename = "resources/team.xml";
+
+	tinyxml2::XMLDocument doc;
+	doc.LoadFile(filename);
+
+	// Load root element of xml
+	tinyxml2::XMLElement* businessElement = doc.FirstChildElement("business");
+
+	// Read from xml and log to console
+	for (tinyxml2::XMLElement* child = businessElement->FirstChildElement("team")->FirstChildElement(); child != NULL; child = child->NextSiblingElement())
+	{
+		std::string firstname = child->FirstChildElement("firstname")->GetText();
+		std::string lastname = child->FirstChildElement("lastname")->GetText();
+		unsigned int age = atoi (child->FirstChildElement("age")->GetText());
+
+		std::cout << firstname << " " << lastname << " is " << age << " years old." << std::endl;
+	}
+
+	// Update an element inside the xml
+	time_t now = std::chrono::system_clock::to_time_t(std::chrono::system_clock::now());
+	businessElement->FirstChildElement("last-update")->SetText(std::ctime(&now));
+	doc.SaveFile(filename);
 }
